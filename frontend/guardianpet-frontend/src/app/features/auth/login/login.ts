d9c8466437eb4d37a29d4e43186f405963debf11
brigith91 <bridgeth91@gmail.com>
2025-10-22 21:07:49 -0500

    login

--- frontend/guardianpet-frontend/src/app/features/auth/login/login.ts
+++ frontend/guardianpet-frontend/src/app/features/auth/login/login.ts
@@ -10,24 +10,12 @@ import { AuthService } from '../../../core/services/auth.service';
   standalone: true,
   selector: 'app-login',
   imports: [CommonModule, FormsModule, RouterLink],
-  template: `
-  <div class="card">
-    <h2>Iniciar sesión</h2>
-    <form (ngSubmit)="onSubmit()">
-      <label>Email</label>
-      <input [(ngModel)]="email" name="email" type="email" required/>
-      <label>Contraseña</label>
-      <input [(ngModel)]="contrasena" name="contrasena" type="contrasena" required/>
-      <button type="submit" [disabled]="loading">Entrar</button>
-    </form>
-    <a routerLink="/registro">Crear cuenta</a>
-    <p class="error" *ngIf="error">{{error}}</p>
-  </div>`,
-  styles:[`.card{max-width:380px;margin:4rem auto;padding:1.5rem;border:1px solid #eee;border-radius:12px} .error{color:#c62828}`]
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
 })
 export class LoginComponent implements OnInit {
   email = '';
-  contrasena = '';     
+  contrasena = '';
   loading = false;
   error = '';
 
