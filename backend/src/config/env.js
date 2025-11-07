6bae9d11894f9a291b150f99e0712602a525f44a
Brigiht91 <bridgeth1991@gmail.com>
2025-11-06 23:33:37 -0500

    Trabajo probado en Postman - versión limpia y estable

--- backend/src/config/env.js
+++
@@ -1,6 +0,0 @@
-export const env = {
-  node: process.env.NODE_ENV || "development",
-  port: Number(process.env.PORT || 4000),
-  jwtSecret: process.env.JWT_SECRET,
-  jwtExp: process.env.JWT_EXPIRES_IN || "1d",
-};
