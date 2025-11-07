a93e107f2341767c369d96a5bff852a62c788394
Brigiht91 <bridgeth1991@gmail.com>
2025-11-07 00:12:27 -0500

    Eliminar carpeta frontend de dentro de backend

--- backend/frontend/guardianpet-frontend/src/app/core/interceptors/jwt.interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const t = localStorage.getItem('gp_token');
-  return next(t ? req.clone({ setHeaders: { Authorization: `Bearer ${t}` } }) : req);
-};
