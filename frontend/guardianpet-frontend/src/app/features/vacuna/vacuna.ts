ceaef22666af7f26eed2f96ee92fdebf7f1c0b04
Miguel Santa <msanta@inter.edu.co>
2025-12-01 19:54:09 -0500

    changes

--- frontend/guardianpet-frontend/src/app/features/vacuna/vacuna.ts
+++ frontend/guardianpet-frontend/src/app/features/vacuna/vacuna.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-
+import { Router,  } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { VacunaService } from '../../core/api/vacuna.service';
 
 
 @Component({
@@ -9,10 +10,34 @@ import { RouterLink } from '@angular/router';
   selector: 'app-vacuna',
   templateUrl: './vacuna.html',
   styleUrls: ['./vacuna.scss'],
-  imports: [RouterLink]
+  imports: [ CommonModule, FormsModule]
 })
 export class VacunaComponent{
-  
-    
 
+    private vac = inject(VacunaService);
+    private router = inject(Router);
+
+    nombre = '';
+    descripcion = '';
+    loading = false;
+    error = '';
+
+    onSubmit() {
+    if (this.loading) return;
+    this.loading = true;
+    this.error = '';
+
+    const payload = {
+      nombre: this.nombre,                // backend: string ≥ 2
+      descripcion: this.descripcion,                    // backend: email válido
+    };
+
+    this.vac.create(payload).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (e: any) => {
+        this.error = e?.error?.error || 'Error de registro';
+        this.loading = false;
+      }
+    });
+  }
 }
